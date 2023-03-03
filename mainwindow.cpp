f3171f36ea3e0c691b5024bbea52a3705c1688c1
Király Márk <69594158+ProgrammerGnome@users.noreply.github.com>
2023-03-03 20:30:02 +0100

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+  : QMainWindow(parent)
+  , ui(new Ui::MainWindow)
+{
+  ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+  delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+  QString text;
+  int number = 10;
+
+  text = ui->lineEdit->text();
+  int num = QVariant(text).toInt() + number;
+
+  ui->plainTextEdit->setPlainText(QVariant(num).toString());
+}
